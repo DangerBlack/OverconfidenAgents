18b8da91ff806b86746682cc87fba2629cacb712
heartnest <granwit@gmail.com>
2014-07-03 14:06:53 +0200

    concurrency solved,simulation cycle bug fixed, code cleaned

--- src/asl/dummy.asl
+++ src/asl/dummy.asl
@@ -2,86 +2,90 @@
 
 /* Initial goals */
 
-/*
-+myString(X):true <- .print(X).
 
-+confidence(X):true <- .print("confidenza",X).*/
++receiveString(X):true <- +myString(X);.print(X).
 
-+confidence(X):true <- .print("SONO ad ",X).
++receiveConfidence(X):true <- +myConfidence(X);.print("confidenza",X).
 
-+comunicate(ME,YOU):confidence(Y) 
-					<- .print("Voglio parlare con ",YOU);
+/* start */
++communicate(ME,YOU):myConfidence(Y) 
+					<- .print("===New conversation started successfully=== \n Voglio parlare con ",YOU);
 						+me(ME);
-						-+you(YOU);
+						+you(YOU);
 						.send(YOU,tell,anotherConfidence(ME,Y)).
 
-+anotherConfidence(YOU,Y):confidence(X) & X>=Y
+/* exchange confidence */
++anotherConfidence(YOU,Y):myConfidence(X) & X>=Y
 					<- .print("Ho ricevuto la confidenza, sono il leader");
-						-+you(YOU);
+						+you(YOU);
 						.send(YOU,tell,leader(no));
 						+leader(si).
-					   
-					   
+					   					   
 					
-+anotherConfidence(YOU,Y):confidence(X) & X<Y
-					<- .print("Ho ricevuto la confidenza, ",YOU," Ã¨ il leader");
-					-+you(YOU);
++anotherConfidence(YOU,Y):myConfidence(X) & X<Y
+					<- .print("Ho ricevuto la confidenza, ",YOU,"  il leader");
+					+you(YOU);
 					+leader(no);
 					.send(YOU,tell,leader(si)).
 					
 					
-			
+/* leader trigger */			
 +leader(si): you(YOU) & myString(S)
-			<- .print("Invio la mia Stringa a ",YOU," leader ",S);
+			<- .print("Leader invia la Stringa a ",YOU," leader ",S);
 			   .send(YOU,tell,anotherString(YOU,S)).
 
+/* exchange string */
 +anotherString(YOU,S2): myString(S1) & leader(no)
 			<- .print("Ho ricevuto la stringa voglio confrontarle ",S1," =?= ",S2);
 				hammingDistance(S1,S2).
 				
 +anotherString(YOU,S2): myString(S1)
 			<- .print("WTF ",S1," =?= ",S2).
-			
-+distance(D): confidence(X) & D<X & you(YOU) & myString(S)
-			<- .print("La distanza Ã¨ ",D," vogliamo lavorare insieme");
+
+/* string distance */			
++distance(D): myConfidence(X) & D<X & you(YOU) & myString(S)
+			<- .print("La distanza  ",D," vogliamo lavorare insieme");
 			   .send(YOU,tell,workTogether(S)).
 
-+distance(D): confidence(X) & D>=X
-			<- .print("La distanza Ã¨ ",D," failure");
-				+concludeCommunication.
-
++distance(D): myConfidence(X) & D>=X
+			<- .print("La distanza  ",D," failure");
+				concludeCommunication.
+/* work trigger */
 +workTogether(S2): myString(S1)
 				<-.print("Confronto con l'ambiente");
 					hammingDistanceEnv(S1).
-					
+
+/* working result */					
 +result(success): workTogether(S2) & myString(S1)
 			<- .print("E' stato un successo");
 			   updateString(S1,S2).
 
 +result(failure):true
-			<- +concludeCommunication.
-			   
-+comunicateUpdate(S): you(YOU)
-			<- .print("Aggiorna la tua credenza");
+			<- concludeCommunication.
+			
+
+/* update results */			   
++communicateUpdate(S): you(YOU)
+			<- .print("Aggiorna la tua credenza del mio partner");
 				.send(YOU,tell,updateMyString(S)).
 
 +updateMyString(S): true
-			<- .print("Aggiorno la stringa ",S);
+			<- .print("Aggiorno la mia stringa ",S);
 			   -+myString(S)[source(percept)];
-			   +concludeCommunication.
+			   concludeCommunication.
 
-+concludeCommunication: you(YOU)
-			<- 	+resetBelief;
-				.send(YOU,tell,concludeCommunicationHard).
 
-+resetBelief: true
-			<-	.print("CANCELLO LE MIE CREDENZE");
-				-comunicate(_,_)[source(_)];
+/* reset believes */
++reset(belief): true
+			<- .print("CANCELLO LE MIE CREDENZE");
+				-communicate(_,_)[source(_)];
 				-anotherConfidence(_,_)[source(_)];
 				-anotherString(_,_)[source(_)];
 				-distance(_)[source(_)];
+				-result(success)[source(_)];
+				-result(failure)[source(_)];
 				-result(_)[source(_)];
-				-comunicateUpdate(_)[source(_)];
+				-communicateUpdate(_)[source(_)];
 				-updateMyString(_)[source(_)];
 				-workTogether(_)[source(_)];
 				-concludeCommunication(_)[source(_)];
@@ -89,9 +93,6 @@
 				-you(_)[source(_)];
 				-leader(_)[source(_)];
 				-resetBelief[source(_)];
-				.print("****CANCELLATE LE MIE CREDENZE").
-				
-+concludeCommunicationHard:true
-			<- 	+resetBelief;
-				-concludeCommunicationHard[source(_)];
-				speechConcluded.
+				 
+				.print("****CANCELLATE LE MIE CREDENZE");
+				informReady.
--- src/java/people/ENV.java
+++ src/java/people/ENV.java
@@ -1,17 +1,36 @@
+/**
+ * ALMA MATER STUDIORUM BOLOGNA - Universita' degli Studi di Bologna
+ * PROGETTO DI Simulazione di sistemi Mod2
+ * Anno Accademico 2013/2014
+ *
+ * Classe L'ambiente della simulazione
+ * 
+ * @author Baschieri Daniele, daniele.baschieri@studio.unibo.it
+ * @author Liu Tong, tong.liu2@studio.unibo.it
+ * @version  0.8
+ */
+
+
 package people;
 
 import jason.asSyntax.*;
 import jason.environment.Environment;
-
 import java.util.logging.Logger;
 import java.util.*;
 
-//import couch.HouseENV;
 
 public class ENV  extends Environment{
+	
+    /*
+     * ====================================================
+     *  		classes
+     * ====================================================
+     * */
+	
 	public class Couple{
 		int a;
 		int b;
+		boolean finished = false;
 		public Couple(int a,int b){
 			this.a=a;
 			this.b=b;
@@ -21,116 +40,218 @@ public class ENV  extends Environment{
 		}
 	}
 	
-	int nStep=10;
+	public class Agent{
+		String myString;
+		int myConfidence;
+
+		public Agent(String s,int c){
+			this.myString=s;
+			this.myConfidence=c;
+		}
+		public String toString(){
+			return "[myString: "+myString+", myConfidence: "+myConfidence+"]";
+		}
+	}
+	
+    /*
+     * ====================================================
+     *  		Variables
+     * ====================================================
+     * */
+	
+	int nStep=3;
 	int step=0;
 	int qoL=3;
-	int nAgents=10;
+	int nAgents;
 	String realString="0000000000";
 	int maxConfidence=9;
 	int minConfidence=8;
-	ArrayList<Couple> chatRoom;
+	int readyAgent = 0;
+	
+	ArrayList<Couple> coupleList;
+	ArrayList<Agent> agentList;
 	
 
     // common literals
+	public static Literal communicate;
+	Literal myString[];
+	Literal confidence[];
 	public final String hd = new String("hammingDistance");
 	public final String hde = new String("hammingDistanceEnv");
 	public final String us = new String("updateString");
-	public final String sc = new String("speechConcluded");
-    //compare(Num,MyStr)
+	public final String cc = new String("concludeCommunication");
+	public final String ir = new String("informReady");
+	
 
-    
     static Logger logger = Logger.getLogger(ENV.class.getName());
+    
+    /*
+     * ====================================================
+     *  		initiation
+     * ====================================================
+     * */
 
     @Override
     public void init(String[] args) {
-		
-		
-    	logger.info("num of agents "+args[0]);
+    	
+    	logger.info("STARTing SIMULATION: \n num of agents "+args[0]);
 		nAgents=Integer.parseInt(args[0]);
 		realString=args[1];
 		logger.info("realString "+realString);
-        updatePercepts();
-		generateChatRoom();
-		doNextCoupleChat();
+		agentList = new ArrayList<Agent>();
+		
+        initPercepts();
+        printAgentsPossession();
+        generateCoupleList();
+		startACycle();
+
     }
     
-	Literal myString[];
-	Literal confidence[];
+	
     /** creates the agents percepts  */
-    void updatePercepts() {
+    void initPercepts() {
 		myString=new Literal[nAgents];
 		confidence=new Literal[nAgents];
+		
 		for(int i=1;i<=nAgents;i++){
-			myString[i-1]= Literal.parseLiteral("myString(\""+generateString()+"\")");
+			String aS = generateString();
+			int aC = generateConfidence();
+			myString[i-1]= Literal.parseLiteral("receiveString(\""+aS+"\")");
 			addPercept("dummy"+i,myString[i-1]);
 			
-			confidence[i-1]= Literal.parseLiteral("confidence("+generateConfidence()+")");
+			confidence[i-1]= Literal.parseLiteral("receiveConfidence("+aC+")");
 			addPercept("dummy"+i,confidence[i-1]);
+			
+			//a copy of mental states
+			Agent tmpAgent = new Agent(aS,aC);
+			agentList.add(tmpAgent);
+     
 		}
     }
-	int generateConfidence(){
-		return (int)(Math.round(Math.random()*(maxConfidence-minConfidence))+minConfidence);
-	}
-    String generateString(){
-		String s="";
-		for(int i=0;i<10;i++){
-			if(Math.random()>0.5){
-				s+="0";
+    
+    /*
+     * ====================================================
+     *  		ACTIONS
+     * ====================================================
+     * */
+	
+    @Override
+    public boolean executeAction(String ag, Structure action) {
+        System.out.println("["+ag+"] doing: "+action);
+        
+        boolean result = true;
+		if (action.getFunctor().equals(hd)) {
+        	/*Calculating hammingDistance */
+			int distance=getHummingDistance(action.getTerm(0).toString(),action.getTerm(1).toString());
+        	logger.info("Calculating HammingDistance !!! term1: "+action.getTerm(0)+" term2:"+action.getTerm(1)+" D="+distance);
+			
+			addPercept(ag,Literal.parseLiteral("distance("+distance+")"));
+        }
+		if (action.getFunctor().equals(hde)) {
+        	/*Calculating hammingDistance */
+			int distance=getHummingDistance(action.getTerm(0).toString().replaceAll("\"",""),realString);
+        	logger.info("Calculating HammingDistanceENV !!! term1: "+action.getTerm(0)+" term2:"+realString+" D="+distance);
+			
+			double punteggio=Math.random()*realString.length();
+			logger.info("Punteggio "+punteggio+" / "+distance);
+			if(punteggio>distance){
+				logger.info("Successo");
+				addPercept(ag,Literal.parseLiteral("result(success)"));
 			}else{
-				s+="1";
+				logger.info("Failure");
+				addPercept(ag,Literal.parseLiteral("result(failure)"));
+			}
+        }
+		if (action.getFunctor().equals(us)) {
+			String newString=getQoLString(action.getTerm(0).toString(),action.getTerm(1).toString());
+			updateEnvCopiedStringByAgentName(ag,newString);
+			logger.info("Calculating updateString !!! term1: "+action.getTerm(0)+" term2:"+action.getTerm(1)+" "+newString);
+			addPercept(ag,Literal.parseLiteral("communicateUpdate("+newString+")"));
+		}
+		if (action.getFunctor().equals(cc)) {
+			//a conversation is finished 
+			Couple p = getCoupleByAgentName(ag);
+			if(!p.finished){
+				addPercept("dummy"+p.a,Literal.parseLiteral("reset(belief)"));
+				addPercept("dummy"+p.b,Literal.parseLiteral("reset(belief)"));
+				p.finished = true;
 			}
 		}
-		return s;
-	}
-	
-	/**
-	*	This method generate the Chat Room in this env
-	*/
-	void generateChatRoom(){
-		ArrayList<Integer> schatRoom = new ArrayList<Integer>();
-		for(int i=1;i<=nAgents;i++)
-		{
-			schatRoom.add(i);
-		}                                       
-		Collections.shuffle(schatRoom);
-		chatRoom = new ArrayList<Couple>();
-		for(int i=0;i<(nAgents-1);i+=2)
-		{
-			chatRoom.add(new Couple(schatRoom.get(i),schatRoom.get(i+1)));
-			logger.info(""+chatRoom.get(chatRoom.size()-1));
+		if (action.getFunctor().equals(ir)) {
+			//an agent cleaned his belief
+			incrementReadyAgent();
 		}
-	}
+        return result;
+    }   
+    
+
 	
-	/**
-		* this method return the next chat room if the chat room is over it return null
-		
-	*/
-	Couple getNextChat(){
-		Couple p;
-		if(chatRoom.size()>0){
-			p=chatRoom.get(0);
-			chatRoom.remove(0);
-			return p;
-		}else{
-			return null;
+    /*
+     * ====================================================
+     *  		simulation cycle
+     * ====================================================
+     * */
+    
+    void startACycle(){
+    	for(int i=0;i<coupleList.size();i++)
+		{
+    		Couple p = coupleList.get(i);
+    		communicate= Literal.parseLiteral("communicate(\"dummy"+p.a+"\" , \"dummy"+p.b+"\")");
+			logger.info("ENV Starts a conversation: "+step+"/"+nStep+" ["+p.a+" => "+p.b+"]");
+			addPercept("dummy"+p.a,communicate);
 		}
-	}
-	public void doNextCoupleChat(){
-		if(step<nStep){			
-			Couple p=getNextChat();
-			if(p==null){
+    }
+	
+    //when a agent ends a conversation
+	synchronized void incrementReadyAgent(){
+		readyAgent++;
+		logger.info("number of ready agents: "+readyAgent+"/"+nAgents);
+		if(readyAgent == nAgents){
+			if(step <nStep){
+			
+				logger.info("ENV Starts Group Conversation: "+step+"/"+nStep);
 				step++;
-				generateChatRoom();
-				p=getNextChat();
+				logger.info("goin to restart chat cyclte ");
+				readyAgent = 0;
+				
+				clearAllPercepts();
+				generateCoupleList();
+				startACycle();
+				
 			}
-			comunicate= Literal.parseLiteral("comunicate(\"dummy"+p.a+"\" , \"dummy"+p.b+"\")");
-			logger.info("FACCIO LO STEP "+step+"/"+nStep+" ["+p.a+" => "+p.b+"]");
-			addPercept("dummy"+p.a,comunicate);
+			printAgentsPossession();
 		}
 	}
-	public static Literal comunicate;
+
+	//update string result
+	void updateEnvCopiedStringByAgentName(String dummy,String newString){
+		int dummyIndex = Integer.parseInt(dummy.replaceAll("dummy", ""));
+		Agent a = agentList.get(dummyIndex-1);
+		a.myString = newString;
+	}
 	
-	public int hummingDistance(String s1,String s2){
+    /*
+     * ====================================================
+     *  		utilities
+     * ====================================================
+     * */
+	
+	Couple getCoupleByAgentName(String dummy){
+		Couple p;
+		for(int i = 0 ; i< coupleList.size(); i++){
+			p=coupleList.get(i);
+			if(dummy.equals("dummy"+p.a) || dummy.equals("dummy"+p.b)){
+				logger.info("trovato");
+				return p;
+			}
+			
+		}
+		logger.info("nn trovato");
+			return null;
+		
+	}
+	
+	public int getHummingDistance(String s1,String s2){
 		
 		if(s1.length()!=s2.length()){
 			System.out.println("ERRORE: #001 STRINGHE DIVERSE "+s1+":"+s1.length()+" "+s2+":"+s2.length());
@@ -146,7 +267,7 @@ public class ENV  extends Environment{
 		s1: the main String of the Leader
 		s2: the String of the follower
 	*/
-	public String updateString(String s1,String s2){
+	public String getQoLString(String s1,String s2){
 		String s="";
 		if(s1.length()!=s2.length()){
 			System.out.println("ERRORE: #002 STRINGHE DIVERSE "+s1+":"+s1.length()+" "+s2+":"+s2.length());
@@ -162,45 +283,51 @@ public class ENV  extends Environment{
 		}
 		return s;		
 	}
-    @Override
-    public boolean executeAction(String ag, Structure action) {
-        System.out.println("["+ag+"] doing: "+action);
-        
-        boolean result = true;
-		if (action.getFunctor().equals(hd)) {
-        	/*Calculating hammingDistance */
-			int distance=hummingDistance(action.getTerm(0).toString(),action.getTerm(1).toString());
-        	logger.info("Calculating HammingDistance !!! term1: "+action.getTerm(0)+" term2:"+action.getTerm(1)+" D="+distance);
-			
-			addPercept(ag,Literal.parseLiteral("distance("+distance+")"));
-        }
-		if (action.getFunctor().equals(hde)) {
-        	/*Calculating hammingDistance */
-			int distance=hummingDistance(action.getTerm(0).toString().replaceAll("\"",""),realString);
-        	logger.info("Calculating HammingDistanceENV !!! term1: "+action.getTerm(0)+" term2:"+realString+" D="+distance);
-			
-			double punteggio=Math.random()*realString.length();
-			logger.info("Punteggio "+punteggio+" / "+distance);
-			if(punteggio>distance){
-				logger.info("Successo");
-				addPercept(ag,Literal.parseLiteral("result(success)"));
+	
+	void printAgentsPossession(){
+		logger.info("===============  START AGENTE REPORT ==============");
+		for(int i = 0 ; i< agentList.size(); i++){
+			Agent a = agentList.get(i);
+			logger.info("DUMMY"+i+": myString: "+a.myString+", myConfidence: "+a.myConfidence);
+		}
+		logger.info("===============  END AGENTE REPORT ==============");
+	}
+	
+    /*
+     * ====================================================
+     *  		generate methods
+     * ====================================================
+     * */
+	
+	void generateCoupleList(){
+		ArrayList<Integer> scoupleList = new ArrayList<Integer>();
+		for(int i=1;i<=nAgents;i++)
+		{
+			scoupleList.add(i);
+		}                                       
+		Collections.shuffle(scoupleList);
+		coupleList = new ArrayList<Couple>();
+		for(int i=0;i<(nAgents-1);i+=2)
+		{
+			coupleList.add(new Couple(scoupleList.get(i),scoupleList.get(i+1)));
+			logger.info(""+coupleList.get(coupleList.size()-1));
+		}
+	}
+    
+    int generateConfidence(){
+		return (int)(Math.round(Math.random()*(maxConfidence-minConfidence))+minConfidence);
+	}
+
+    String generateString(){
+		String s="";
+		for(int i=0;i<10;i++){
+			if(Math.random()>0.5){
+				s+="0";
 			}else{
-				logger.info("Failure");
-				addPercept(ag,Literal.parseLiteral("result(failure)"));
+				s+="1";
 			}
-			//addPercept(ag,Literal.parseLiteral("distance("+distance+")"));
-        }
-		if (action.getFunctor().equals(us)) {
-			String newString=updateString(action.getTerm(0).toString(),action.getTerm(1).toString());
-			logger.info("Calculating updateString !!! term1: "+action.getTerm(0)+" term2:"+action.getTerm(1)+" "+newString);
-			addPercept(ag,Literal.parseLiteral("comunicateUpdate("+newString+")"));
 		}
-		if (action.getFunctor().equals(sc)) {
-			logger.info("Finito  ");
-			doNextCoupleChat();
-			//clearPercepts(ag);
-		}
-        return result;
-    }   
+		return s;
+	}
 
 }
